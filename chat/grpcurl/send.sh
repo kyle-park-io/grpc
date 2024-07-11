fe12bc06d6e65f8ed7dae481cdd64ec9fcb57968
kyle-park-io <andy3638@naver.com>
2024-07-11 21:29:21 +0900

    chore: update grpcurl script

--- chat/grpcurl/send.sh
+++ chat/grpcurl/send.sh
@@ -3,3 +3,6 @@
 grpcurl -plaintext -d '{"id": 1}' -proto ../proto/chat.proto 127.0.0.1:50051 kyle_chat.Chat/SendMsgTest
 
 grpcurl -plaintext -d '{"id": 1}' -proto ../proto/chat.proto 127.0.0.1:50051 kyle_chat.Chat/GetMsgTest
+
+# envoy
+grpcurl -plaintext -d '{"id": 1}' -proto ../proto/chat.proto 127.0.0.1:8080 kyle_chat.Chat/SendMsgTest
