4e5b2179daf1dd69ce32973bb18de1524f98bf7e
kyle-park-io <andy3638@naver.com>
2024-07-11 21:27:40 +0900

    feat: add deployment script for GKE

---
+++ chat/push2gke_artifact.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+PROJECT_ID=kyle-server-402706
+REPOSITORY=kyle-registry
+LOCATION=me-west1
+IMAGE=chat-node-server
+TAG=0.0.1
+TAG_LATEST=latest
+
+# dir
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+DOCKERFILE_PATH=$SCRIPT_DIR/Dockerfile
+CONTEXT_PATH=$SCRIPT_DIR
+
+set -e
+
+# delete existing image
+# untag
+gcloud artifacts docker tags delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG --quiet || true
+gcloud artifacts docker tags delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG_LATEST --quiet || true
+# delete
+# --filter="tags:$TAG" --format="get(DIGEST)" --limit=1
+DIGEST=$(gcloud artifacts docker images list $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY --filter="PACKAGE: $IMAGE" --format="get(DIGEST)" --limit=1)
+gcloud artifacts docker images delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE@$DIGEST --quiet || true
+
+# IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/chat-node-server:0.0.1
+IMAGE_TAG=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG
+# --no-cache
+docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
+docker push $IMAGE_TAG
+
+IMAGE_TAG_LATEST=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG_LATEST
+docker tag $IMAGE_TAG $IMAGE_TAG_LATEST
+docker push $IMAGE_TAG_LATEST
