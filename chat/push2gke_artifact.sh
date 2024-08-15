907ed9070300911b3ea290d1540935f97295a59c
kyle-park-io <andy3638@naver.com>
2024-08-15 18:35:19 +0900

    refactor: apply Pod and Service name changes to GKE deployment

--- chat/push2gke_artifact.sh
+++ chat/push2gke_artifact.sh
@@ -3,14 +3,15 @@
 PROJECT_ID=kyle-server-402706
 REPOSITORY=kyle-registry
 LOCATION=me-west1
-IMAGE=chat-node-server
+IMAGE=chat-app-server
 TAG=0.0.1
 TAG_LATEST=latest
 
 # dir
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-DOCKERFILE_PATH=$SCRIPT_DIR/Dockerfile
-CONTEXT_PATH=$SCRIPT_DIR
+echo $SCRIPT_DIR
+DOCKERFILE_PATH=$SCRIPT_DIR/server/Dockerfile
+CONTEXT_PATH=$SCRIPT_DIR/server
 
 set -e
 
@@ -23,10 +24,11 @@ gcloud artifacts docker tags delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSI
 DIGEST=$(gcloud artifacts docker images list $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY --filter="PACKAGE: $IMAGE" --format="get(DIGEST)" --limit=1)
 gcloud artifacts docker images delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE@$DIGEST --quiet || true
 
-# IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/chat-node-server:0.0.1
+# IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/chat-app-server:0.0.1
 IMAGE_TAG=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG
 # --no-cache
-docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
+docker buildx build --no-cache --build-arg=PROGRAM_VER=0.0.1 --load -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
+# docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 --push -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
 docker push $IMAGE_TAG
 
 IMAGE_TAG_LATEST=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG_LATEST
